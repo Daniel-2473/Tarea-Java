5a816c1454b2f40ff8dfea77a9330e47e33c2761
Daniel-2473 <daniel.ulate937@gmail.com>
2025-06-04 16:56:04 -0600

    Agregar un menu de opciones a mostrar en la terminal

--- main.java
+++ main.java
@@ -1,5 +1,20 @@
-public class main {
-    
+public class Main {
+   public static void imprimir_opciones() {
+    System.out.println("Calculadora de funciones trigonométricas");
+    System.out.println("1: Seno (sen)"); //La idea es que el usuario ingrese algún número para seleccionar la función
+    System.out.println("2: Coseno (cos)");
+    System.out.println("3: Tangente (tan)");
+    System.out.println("4: Arcoseno (arcsen)");
+    System.out.println("5: Arcocoseno (arccos)");
+    System.out.println("6: Arcotangente (arctan)");
+    System.out.println("7: Seno hiperbólico (senh)");
+    System.out.println("8: Coseno hiperbólico (cosh)");
+    System.out.println("9: Tangente hiperbólico (tanh)");
+   }
+
+   public static void main(String[] args){
+    imprimir_opciones();
+   }
 }
 
 class funciones_hiperblicas {
